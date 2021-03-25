9142338acb368c043aa8d2e1b2805c91989da383
MaGic-IT-VN <fifamatac2015@gmail.com>
2021-03-25 15:55:35 +0700

    Update core code & Green 4ever

--- test/com/mouse/mathutil/test/MathUtilityTest.java
+++ test/com/mouse/mathutil/test/MathUtilityTest.java
@@ -35,8 +35,7 @@ public class MathUtilityTest {
         assertEquals(24, MathUtility.getFactorial(4));
         assertEquals(6, MathUtility.getFactorial(3));
         assertEquals(2, MathUtility.getFactorial(2));
-        assertEquals(1, MathUtility.getFactorial(1));
-        assertEquals(1, MathUtility.getFactorial(0));
+        
         //0! là 1 tình huống cần test
         //1! là 1 tính huống khác cần test
         //6! là 1 tình huống khác cần test
