786c905608e42d16d497cf51c5f311c800d6e2a8
MaGic-IT-VN <fifamatac2015@gmail.com>
2021-03-25 16:34:06 +0700

    RECURSION UPDATE & GREEN 4EVER AGAIN

--- src/com/mouse/mathutil/MathUtility.java
+++ src/com/mouse/mathutil/MathUtility.java
@@ -23,12 +23,22 @@ public class MathUtility {
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("n must be between 0..20");
         //sống sót đến đây thì n = 0, 1,...20
-        if(n ==0 || n==1)
-            return 1; //0!, 1! = 1
-        long result = 1; //xanh local và server // giả bộ = 0
-        for(int i = 2; i <= n; i++)
-            result *=i;
-        return result; // nhân từ 2 3 4 5 6 7....
+        
+        //sửa code, sửa hàm giai thừa cho gọn, dùng đệ quy
+        if(n==0||n==1)
+            return 1; //0! = 1! = 1 tính luôn cho nhanh
+        return n *getFactorial(n-1);
+        //n! = n * (n-1)!;
+        //đệ quy, vòng lại chính mình với quy mô khaac1
+        //recursion. Làm khéo nếu ko nó đi mãi mãi
+        //STACKOVERLOW.com
+        
+//        if(n ==0 || n==1)
+//            return 1; //0!, 1! = 1
+//        long result = 1; //xanh local và server // giả bộ = 0
+//        for(int i = 2; i <= n; i++)
+//            result *=i;
+//        return result; // nhân từ 2 3 4 5 6 7....
           
     }
 }
